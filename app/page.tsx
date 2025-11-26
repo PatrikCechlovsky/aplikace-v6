0e90b17c9fd78d4332fa7a138f5810a63bce3ffe
PatrikCechlovsky <patrik.cechlovsky@centrum.cz>
2025-11-26 18:05:38 +0100

    Replace Tabs and DetailView with LoginPanel

--- app/page.tsx
+++ app/page.tsx
@@ -2,25 +2,26 @@
 
 'use client'
 
+import LoginPanel from './UI/LoginPanel'
+
+// Importy necháme odkomentované, protože se používají ↓
+
 import HomeButton from './UI/HomeButton'
 import Sidebar from './UI/Sidebar'
 import Breadcrumbs from './UI/Breadcrumbs'
 import HomeActions from './UI/HomeActions'
 import CommonActions from './UI/CommonActions'
-import Tabs from './UI/Tabs'
-import DetailView from './UI/DetailView'
-// import ListView from './UI/ListView' // použijeme později
 
 export default function HomePage() {
   return (
     <div className="flex min-h-screen bg-gray-100">
 
-      {/* BLOK 2 – Sidebar (dynamický, načítá moduly z modules.index.js) */}
+      {/* BLOK 2 – Sidebar */}
       <Sidebar />
 
       <div className="flex-1 flex flex-col">
 
-        {/* BLOK 1 + 4 – HomeButton vlevo, HomeActions vpravo */}
+        {/* BLOK 1 + 4 – HomeButton & HomeActions */}
         <div className="flex justify-between items-center px-4 h-12 bg-white border-b">
           <HomeButton />
           <HomeActions />
@@ -31,17 +32,14 @@ export default function HomePage() {
           <Breadcrumbs />
         </div>
 
-        {/* BLOK 5 – CommonActions (zatím statické, později dynamické) */}
+        {/* BLOK 5 – CommonActions */}
         <div className="px-4 py-2 border-b bg-white">
           <CommonActions />
         </div>
 
-        {/* BLOK 6 – Tabs (10 záložek) + Detail hlavní karty */}
+        {/* BLOK 6 – Content s LOGINEM */}
         <div className="flex-1 p-4 overflow-y-auto">
-          <Tabs />
-          <div className="mt-4">
-            <DetailView />
-          </div>
+          <LoginPanel />
         </div>
       </div>
     </div>
