7b2d971f05f191274a33b69d40a91d84fa38b12d
PatrikCechlovsky <patrik.cechlovsky@centrum.cz>
2025-11-26 18:36:38 +0100

    Comment out specific module imports
    Comment out imports for 'udrzba' and 'help' modules.

--- app/modules.index.js
+++ app/modules.index.js
@@ -12,9 +12,9 @@ export const MODULE_SOURCES = [
   () => import('./modules/080-platby/module.config.js'),
   () => import('./modules/090-finance/module.config.js'),
   () => import('./modules/100-energie/module.config.js'),
-  () => import('./modules/110-udrzba/module.config.js'),
+  // () => import('./modules/110-udrzba/module.config.js'),
   () => import('./modules/120-dokumenty/module.config.js'),
   () => import('./modules/130-komunikace/module.config.js'),
   () => import('./modules/900-nastaveni/module.config.js'),
-  () => import('./modules/990-help/module.config.js'),
+  // () => import('./modules/990-help/module.config.js'),
 ]
