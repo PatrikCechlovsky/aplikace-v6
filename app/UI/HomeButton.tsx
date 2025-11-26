82b61994f0daf28270b829fa8bb5597c9b9d86e0
PatrikCechlovsky <patrik.cechlovsky@centrum.cz>
2025-11-26 21:48:30 +0100

    Add disabled prop to HomeButton component

--- app/UI/HomeButton.tsx
+++ app/UI/HomeButton.tsx
@@ -1,9 +1,13 @@
 // app/UI/HomeButton.tsx
 'use client'
 
-export default function HomeButton() {
+type Props = {
+  disabled?: boolean
+}
+
+export default function HomeButton({ disabled = false }: Props) {
   return (
-    <div className="home-button">
+    <div className={`home-button ${disabled ? 'is-disabled' : ''}`}>
       <span className="home-button__icon">🏠</span>
       <span className="home-button__text">Pronajímatel v6</span>
     </div>
