857d3b15109c28a1475be48a7b2c46797e86f54d
PatrikCechlovsky <patrik.cechlovsky@centrum.cz>
2025-11-26 14:14:46 +0100

    Add module configuration for user management

---
+++ app/modules/010-sprava-uzivatelu/module.config.js
@@ -0,0 +1,11 @@
+// FILE: src/app/modules/010-sprava-uzivatelu/module.config.js
+
+export default {
+  id: '010-sprava-uzivatelu',
+  order: 10,
+  label: 'Správa uživatelů',
+  icon: '🧑‍💻',
+  enabled: true,
+
+  // později: přehledy, formuláře, tiles, actions...
+}
