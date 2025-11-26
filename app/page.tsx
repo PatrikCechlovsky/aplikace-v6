f1147df38b24ece1b2cc168169476e728623a509
PatrikCechlovsky <patrik.cechlovsky@centrum.cz>
2025-11-26 21:28:58 +0100

    Refactor HomePage layout and component imports

--- app/page.tsx
+++ app/page.tsx
@@ -2,46 +2,37 @@
 
 'use client'
 
-import LoginPanel from './UI/LoginPanel'
-
-// Importy necháme odkomentované, protože se používají ↓
-
-import HomeButton from './UI/HomeButton'
 import Sidebar from './UI/Sidebar'
-import Breadcrumbs from './UI/Breadcrumbs'
+import HomeButton from './UI/HomeButton'
 import HomeActions from './UI/HomeActions'
+import Breadcrumbs from './UI/Breadcrumbs'
 import CommonActions from './UI/CommonActions'
+import LoginPanel from './UI/LoginPanel'
 
 export default function HomePage() {
   return (
-    <div className="flex min-h-screen bg-gray-100">
-
-      {/* BLOK 2 – Sidebar */}
-      <Sidebar />
-
-      <div className="flex-1 flex flex-col">
-
-        {/* BLOK 1 + 4 – HomeButton & HomeActions */}
-        <div className="flex justify-between items-center px-4 h-12 bg-white border-b">
-          <HomeButton />
-          <HomeActions />
-        </div>
-
-        {/* BLOK 3 – Breadcrumbs */}
-        <div className="px-4 py-2 border-b bg-white">
-          <Breadcrumbs />
-        </div>
-
-        {/* BLOK 5 – CommonActions */}
-        <div className="px-4 py-2 border-b bg-white">
-          <CommonActions />
-        </div>
-
-        {/* BLOK 6 – Content s LOGINEM */}
-        <div className="flex-1 p-4 overflow-y-auto">
-          <LoginPanel />
-        </div>
+    <div className="layout">
+      {/* Levý sloupec: 1 = HomeButton, 2 = Sidebar */}
+      <div className="layout__sidebar">
+        <HomeButton />
+        <Sidebar />
       </div>
+
+      {/* Horní lišta vpravo: 3 = Breadcrumbs, 4 = HomeActions */}
+      <header className="layout__topbar">
+        <Breadcrumbs />
+        <HomeActions />
+      </header>
+
+      {/* Lišta akcí: 5 = CommonActions */}
+      <div className="layout__actions">
+        <CommonActions />
+      </div>
+
+      {/* Hlavní obsah: 6 = Content (teď login) */}
+      <main className="layout__content">
+        <LoginPanel />
+      </main>
     </div>
   )
 }
